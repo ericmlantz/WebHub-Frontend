6eab65d98dfd525224b46284f6372514c9a58291
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-09 12:00:21 -0400

    Set up thum api calls

--- src/pages/Interests.js
+++ src/pages/Interests.js
@@ -5,10 +5,9 @@ const Interests = () => {
   let auth = '57899-2f651113cbc45483ba2e7f8c5b935874';
   let thumUrl = '';
 
-  let imgUrl = '//image.thum.io/get/auth/' + auth + '/' + thumUrl;
+  let imgUrl = '//image.thum.io/get/fullpage/auth/' + auth + '/' + thumUrl;
 
   return (
-    
     <div className='interest-page'>
         <form className="search-wrapper mb-3 mb-lg-0 me-lg-3">
           <input type="search" className="form-control form-control-dark search" placeholder="Search..." aria-label="Search"/>
@@ -16,27 +15,10 @@ const Interests = () => {
       <div className="row row-cols-1 row-cols-md-2 g-4">
         <div className="col">
           <div className="card">
+          <img src={imgUrl} className="card-img-top" alt="screenshot"/>
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">{page_title}</h5>
               <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
             </div>
           </div>
         </div>
