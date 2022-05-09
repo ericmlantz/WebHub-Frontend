2377d4b3389c5d61e87efbf9cf3de40ae940c8c1
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-09 09:30:52 -0400

    Created Component and Pages folders and the included components and pages inside them

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './style/App.css';
-import html2canvas from 'html2canvas';
+
+import Home from './pages/Home'
+import Interests from './pages/Interests'
+import NavBar from './components/NavBar'
 
 function App() {
   
-  html2canvas(document.querySelector("#capture")).then(canvas => {
-    document.body.appendChild(canvas)
-});
+
 
   return (
     <div className="App">
-      <button id='but_screenshot' value='Take screenshot' onClick='screenshot();'>
-      </button>
+      <NavBar/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './style/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
