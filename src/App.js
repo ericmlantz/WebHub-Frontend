af92c40b740d2808716e24bb182373430a707e71
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-09 08:53:11 -0400

    Created style folder for css and did react-router-dom install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import html2canvas from 'html2canvas';
 
 function App() {
+  
+  html2canvas(document.querySelector("#capture")).then(canvas => {
+    document.body.appendChild(canvas)
+});
+
   return (
     <div className="App">
+      <button id='but_screenshot' value='Take screenshot' onClick='screenshot();'>
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,3 +11,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
